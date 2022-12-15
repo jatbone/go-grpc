d4dd17bc742dbeef1b0d7f8e71916e5a7e0b463a
Jakub <jakub.tbone@gmail.com>
2022-12-15 22:19:06 +0100

    grcp server init

--- server/main.go
+++ server/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/jatbone/go-grpc/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+
+type helloServer struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	list, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("Failed to start server %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("server started at %v", list.Addr())
+
+	if err := grpcServer.Serve(list); err != nil {
+		log.Fatalf("Failed to start %v", err)
+	}
+
+}
